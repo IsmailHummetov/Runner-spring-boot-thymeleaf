67d518e609281955843cb3361ada44443d1e77a5
Ismail Hummetov <adnsu.ismail2003@gmail.com>
2024-07-03 15:16:05 +0400

    Security

--- src/main/java/com/example/Runner/security/SecurityConfig.java
+++ src/main/java/com/example/Runner/security/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -32,13 +33,15 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> {
                     auth
-                            .requestMatchers("/login", "/register", "/clubs", "/events","/css/**","/js/**")
+                            .requestMatchers("/","/login", "/register", "/clubs", "/events","/css/**","/js/**")
+                            .permitAll()
+                            .requestMatchers("/clubs/*","/events/*")
                             .permitAll()
                             .anyRequest()
-                            .permitAll();
+                            .authenticated();
                 })
                 .formLogin(form -> form
                         .loginPage("/login")
